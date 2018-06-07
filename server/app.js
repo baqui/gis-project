1ce4bcd7da124e007fcdc1c92cc8aa3d7292f77b
Wojciech <wojtek@qunabu.com>
2018-06-07 22:22:35 +0200

    remove unsued variables

--- server/app.js
+++ server/app.js
@@ -14,9 +14,7 @@ app.use(async (req, res, next) => {
   const start = Date.now();
   await next();
   const responseTime = Date.now() - start;
-  console.log.info(
-    `${ctx.method} ${ctx.status} ${ctx.url} - ${responseTime} ms`
-  );
+  console.log.info(`$Server response: ${responseTime} ms`);
 });
 
 app.listen(PORT, () => {
